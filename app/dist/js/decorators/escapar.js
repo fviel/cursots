31bf18ce7d1879c7d52c09b1de0ad49d62f99432
fviel <nandoviel@hotmail.com>
2023-02-20 17:06:33 -0300

    correção de bug no decorator de escapar

--- app/dist/js/decorators/escapar.js
+++ app/dist/js/decorators/escapar.js
@@ -1,10 +1,11 @@
 export function escapar(target, key, descriptor) {
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args) {
-        let retorno = descriptor.value.apply(this, args);
+        let retorno = metodoOriginal.apply(this, args);
         if (typeof retorno === 'string') {
             retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
         }
+        console.log(`Decorator escapar: retorno: ${JSON.stringify(retorno)}`);
         return retorno;
     };
     return descriptor;
--- app/src/decorators/escapar.ts
+++ app/src/decorators/escapar.ts
@@ -2,19 +2,23 @@ export function escapar(
     target: any,
     key: String,
     descriptor: PropertyDescriptor) {
-        const metodoOriginal = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-
-            //minha tentativa fazendo o replane no args, mas o professor fez o replace no retorno
-            //args = args.map(itemArgs => {
-            //    itemArgs = itemArgs.replace(/<script>[\s\S]*?<\/script>/, '');
-            //});
-
-            let retorno = descriptor.value.apply(this, args);
-            if (typeof retorno === 'string') {
-                retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
-            }
-            return retorno;
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        //Na linha abaixo não pode usar descriptor.value, caso contrário irá gerar um loop infinito que irá exceder a pilha de chamadas
+        let retorno = metodoOriginal.apply(this, args);
+        if (typeof retorno === 'string') {
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
         }
+        console.log(`Decorator escapar: retorno: ${JSON.stringify(retorno)}`);
+        return retorno;
+    }
     return descriptor;
 }
+
+/*
+    Exemplo de map:
+            minha tentativa fazendo o replane no args, mas o professor fez o replace no retorno
+            args = args.map(itemArgs => {
+                itemArgs = itemArgs.replace(/<script>[\s\S]*?<\/script>/, '');
+            });
+*/
